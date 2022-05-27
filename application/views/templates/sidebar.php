4503c4bf4110abb5eb1836bf1b9df79b2e073e81
rafi-windows <rafi_n_o@github.com>
2022-05-27 13:18:17 +0700

    USER ACCESS

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -43,13 +43,13 @@
         <?php foreach ($subMenu as $sm) : ?>
             <?php if ($title == $sm['title']) : ?>
                 <li class="nav-item active">
-                    <a class="nav-link" href="<?= base_url($sm['url']) ?>">
+                    <a class="nav-link pb-0" href="<?= base_url($sm['url']) ?>">
                         <i class="<?= $sm['icon'] ?>"></i>
                         <span><?= $sm['title'] ?></span></a>
                 </li>
             <?php else : ?>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?= base_url($sm['url']) ?>">
+                    <a class="nav-link pb-0" href="<?= base_url($sm['url']) ?>">
                         <i class="<?= $sm['icon'] ?>"></i>
                         <span><?= $sm['title'] ?></span></a>
                 </li>
@@ -57,7 +57,7 @@
         <?php endforeach ?>
 
         <!-- Divider -->
-        <hr class="sidebar-divider">
+        <hr class="sidebar-divider mt-3">
     <?php endforeach ?>
 
     <!-- Nav Item - Logout -->
